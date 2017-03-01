ea4cbb8f83ae76923880e01d2444fffc2dcafcf3
Brett Walker <bdwalker93@yahoo.com>
2017-03-01 01:03:44 -0800

    added id's to index and added various error handling/exception checking

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -38,10 +38,12 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.BytesRef;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
@@ -49,18 +51,37 @@ import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 public class SearchEngine {
 
 	final private static boolean PRINT_INDEX_TO_SCREEN = true;
-	final private static boolean PRINT_INDEX_TO_FILE = true;
+	final private static boolean PRINT_INDEX_TO_FILE = false;
+	
+	final private static boolean GET_CONTENT_URL = false;
+	final private static boolean PRINT_CONTENT_STRING = false;
+	final private static boolean PRINT_CONTENT_BODY = false;
+	final private static boolean PRINT_CONTENT_TEXT = true;
 
 	final private static boolean USE_REAL_FILES = true;
-	final private static int REAL_FILE_INDEX_LIMIT = 100;
+	final private static int REAL_FILE_INDEX_LIMIT = -1;
 	
-
+	final private static String REAL_INDEX = "index";
+	final private static String HUMAN_READABLE_INDEX = "index.txt";
+	
 	public static void main(String[] args) throws IOException, ParseException{
 		 StandardAnalyzer analyzer = new StandardAnalyzer(); 
 		 
 		 //TODO: This is not a persistant index. We need to find something that stores the index. http://stackoverflow.com/questions/9146604/how-to-persist-the-lucene-document-index-so-that-the-documents-do-not-need-to-be
-		 Directory index = new RAMDirectory();																						
-		 IndexWriterConfig config = new IndexWriterConfig(analyzer);	
+		 
+		 File indexFile = new File(REAL_INDEX);
+		 
+		 //Check to make sure the index directory exists
+		 if(!indexFile.isDirectory())
+		 {
+			indexFile.mkdir(); 
+		 }
+		 
+		 Directory index = new SimpleFSDirectory(indexFile.toPath());																						
+		 IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		 
+		 //Sets how we handles an existing index
+		 config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 		 IndexWriter w = new IndexWriter(index, config);				
 		 
 		 //URL path = Test_Lucene.class.getResource("SampleTextDoc.txt"); //How to get txt that is in same directory to avoid complications
@@ -75,11 +96,18 @@ public class SearchEngine {
 			 JSONArray nameArr = jsonObj.names();
 			 
 			 // Traverse our bookeeping JSON file that has all of the paths of the files for us to index
-			 for(int i = 0; i < nameArr.length() && i < REAL_FILE_INDEX_LIMIT; i++)
+			 for(int i = 0; i < nameArr.length() && i < REAL_FILE_INDEX_LIMIT || REAL_FILE_INDEX_LIMIT == -1; i++)
 			 {
-				 System.out.println("Currently Parsing: WEBPAGES_RAW/" + (String)nameArr.get(i));
+				 System.out.println("\nCurrently Parsing #" + i + " : WEBPAGES_RAW/" + (String)nameArr.get(i) + (GET_CONTENT_URL ? " -- This is the URL: " + jsonObj.getString((String)nameArr.get(i)) : ""));
+				 
 				 inputFile = new File("WEBPAGES_RAW/" + (String)nameArr.get(i));
+				 
+				 try{
 				 addDoc(w, inputFile);
+				 }catch(IllegalArgumentException e)
+				 {
+					 System.out.println("***ILLEGAL ARGUMENTS FOUND***: " + e.getMessage());
+				 }
 			 }
 		 }
 		 else
@@ -108,15 +136,39 @@ public class SearchEngine {
 	
 	
 	//Will add new document to Index
-	private static void addDoc(IndexWriter w, File file) throws IOException {
+	private static void addDoc(IndexWriter w, File file) throws IOException, IllegalArgumentException {
 		
 		//TODO: needs to be able to parse HTML pages here
 		//File parsing
 		org.jsoup.nodes.Document html = Jsoup.parse(String.join("",Files.readAllLines(file.toPath())));
 		
+		//If we cant parse the html
+		if(html == null)
+			return;
 		
-		String title = file.getName();
+		if(PRINT_CONTENT_STRING)
+			System.out.println("***This is the body***\n" + String.join("",Files.readAllLines(file.toPath())));
 		
+		if(PRINT_CONTENT_BODY)
+			System.out.println("***This is the body***\n" + html.body());
+		
+		String content = null;
+		Element body = html.body();
+		
+		if(body != null)
+			content = body.text();
+		
+		if(PRINT_CONTENT_TEXT)
+			System.out.println("***This is the BODY text***\n" + content);
+
+		String title = null;
+		Element head = html.head();
+		if(head != null)
+			title = head.text();
+		
+		if(PRINT_CONTENT_TEXT)
+			System.out.println("***This is the TITLE***\n" + title);
+				
 		//Document Creation
 		Document doc = new Document();
 		FieldType type = new FieldType();
@@ -124,9 +176,17 @@ public class SearchEngine {
 		type.setStored(true); 
 		type.setStoreTermVectors(true);
 		type.setTokenized(true);
-		  
-		doc.add(new Field("title", title, type));
-//		doc.add(new Field("content", content, type));
+		
+		//If there is text in the head, it is probably a title
+		if(title != null && !title.isEmpty())
+			doc.add(new Field("title", title, type));
+		
+		//Need to make sure there is a body and it isnt empty
+		if(content != null && !content.isEmpty())
+			doc.add(new Field("content", content, type));
+		
+		doc.add(new Field("fileId", file.getName(), type));
+		
 		w.addDocument(doc);
 	}
 	
@@ -154,10 +214,10 @@ public class SearchEngine {
 	            	 
 	            	 if (hmap.containsKey(strTerm)){
 	            		 list = hmap.get(strTerm);
-	            		 list.add(doc.get("title"));
+	            		 list.add(doc.get("fileId"));
 	            	 } else{
 	            		 list = new ArrayList<String>();
-	            		 list.add(doc.get("title"));
+	            		 list.add(doc.get("fileId"));
 	            		 hmap.put(strTerm, list);
 	            	 }
 	             }
@@ -167,9 +227,14 @@ public class SearchEngine {
 		 return hmap;
 	}
 	
+	private static void getMetrics()
+	{
+//		long fileSizeInKB = "this needs to be all of our cfs files" / 1024;
+
+	}
+	
 	//Iterates through map and prints out as a postings as seen in lectures
 	private static void printOutIndex(HashMap<String, ArrayList<String>> hmap){
-		String outputFileName = "index.txt";
 		
 		//creating output string
 		String output = new String();
@@ -187,7 +252,7 @@ public class SearchEngine {
        	//Prints index to a file (based on the print boolean control)
        	if(PRINT_INDEX_TO_FILE){
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream(outputFileName), "utf-8"))) {
+                    new FileOutputStream(HUMAN_READABLE_INDEX), "utf-8"))) {
             	writer.write(output);
             }catch(Exception e){}
        	}
